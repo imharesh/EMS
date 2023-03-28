69f3bc6364c5acdc4b080242277bd2557a1e5ec2
imharesh <128148066+imharesh@users.noreply.github.com>
2023-03-28 16:22:10 +0530

    add

---
+++ src/employee.Web/Pages/Emps/Index.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+    var l = abp.localization.getResource('employee');
+
+    var dataTable = $('#EmpsTable').DataTable(
+        abp.libs.datatables.normalizeConfiguration({
+            serverSide: true,
+            paging: true,
+            order: [[1, "asc"]],
+            searching: false,
+            scrollX: true,
+            ajax: abp.libs.datatables.createAjax(employee.emps.emp.getList),
+            columnDefs: [
+                {
+                    title: l('Name'),
+                    data: "name"
+                },
+                {
+                    title: l('Type'),
+                    data: "type",
+                    render: function (data) {
+                        return l('Enum:Department.' + data);
+                    }
+                },
+
+                {
+                    title: l('Age'),
+                    data: "age"
+                },
+                {
+                    title: l('Salary'),
+                    data: "salary"
+                },
+               
+            ]
+        })
+    );
+});
